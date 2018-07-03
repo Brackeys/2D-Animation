2e80e2a59a95c7b1767fd0bea18b6aa4d74279fe
ATBrackeys <at@brackeys.com>
2018-07-03 12:39:03 +0200

    Initial commit

---
+++ 2D Animation/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+	public Animator animator;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+
+	// Update is called once per frame
+	void Update () {
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+			animator.SetBool("IsJumping", true);
+		}
+
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		} else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+
+	}
+
+	public void OnLanding ()
+	{
+		animator.SetBool("IsJumping", false);
+	}
+
+	public void OnCrouching (bool isCrouching)
+	{
+		animator.SetBool("IsCrouching", isCrouching);
+	}
+
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# 2D-Animation
-Project files for our tutorial on animating a 2D Character in Unity.
+# 2D Animation
+Project files for our tutorial on 2D Animation in Unity.
+
+The complete Unity project is under "2D Animation" and the newest version of the CharacterController2D can be found [here](https://github.com/Brackeys/2D-Character-Controller).
+
+The asset pack used for the environment is Sunny Land which you can download [here](https://assetstore.unity.com/packages/2d/characters/sunny-land-103349).
+
+Check out our [YouTube Channel](http://youtube.com/brackeys) for more tutorials.
